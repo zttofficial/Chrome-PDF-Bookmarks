c99ccc3a30aff5f4b2f1e16532aa7235a120e095
Harold Lee <zttofficial@users.noreply.github.com>
2021-01-22 00:09:35 +0800

    basic interface

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Chrome-PDF-Bookmarks
 Add bookmarks to the native Chrome PDF viewer
+
+
+
+### TODO
+
+There is not a reliable way to retrieve current page number in native Chrome PDF viewer.
+
--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
+
+/* demo code from official document
+
 let changeColor = document.getElementById('changeColor');
 
 changeColor.onclick = function(element) {
@@ -9,7 +12,28 @@ changeColor.onclick = function(element) {
   });
 };
 
+
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
+*/
+
+let currentPage = document.getElementById('Bookmark_save');
+
+Bookmark_load.onclick = function(element) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+		tabs[0].id,
+        {code:'let current_url = tabs[0].url;let bookmark = current_url+ "#page=" + currentPage;window.open(bookmark);'});
+  });
+};
+
+/*
+test.onclick = function(element) {
+	chrome.browserAction.onClicked.addListener(function(activeTab){
+		var newURL = "http://stackoverflow.com/";
+		chrome.tabs.create({ url: newURL });
+	});
+};
+*/
